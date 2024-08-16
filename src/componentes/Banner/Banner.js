c044863a1ccb5c7d2410784bfd3371cd72bd776a
Gabriel Pedroso <gabrielpedrosor@gmail.com>
2024-08-16 12:59:28 -0300

    Firts steps in React.JS

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="BannerPrincipal" />
+        </header>    
+    )
+}
+
+export default Banner
