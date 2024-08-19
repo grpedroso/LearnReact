b22cbac1e1f414ee0c076159dc61db7c9ad93e88
Gabriel Pedroso <gabrielpedrosor@gmail.com>
2024-08-19 17:28:11 -0300

    Forms created and adjust in background image

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,13 +1,31 @@
+import Botao from "../Botao"
 import CampoTexto from "../CampoTexto"
+import ListaSuspensa from "../ListaSuspensa"
 import './formulario.css'
+
 const Formulario = () => {
+
+    const rotas = [
+         'TOP',
+         'JUNGLE',
+         'MID',
+         'ADC',
+         'SUP'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log('Form submetido')
+    }
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2> Preencha os dados do campeão</h2>
-                <CampoTexto label="Nome" placeholder='Digite seu nome' />
-                <CampoTexto label="Rota" placeholder='Digite sua rota' />
-                <CampoTexto label="Imagem" placeholder='Digite o link da imagem' />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder='Digite o nome' />
+                <CampoTexto obrigatorio={true} label="Classe" placeholder='Digite a classe do campeão' />
+                <CampoTexto obrigatorio={true} label="Imagem" placeholder='Digite o link da imagem' />
+                <ListaSuspensa label="Rota/Lane" itens={rotas}/>
+                <Botao texto="Adicionar" />
             </form>
         </section>
     )
